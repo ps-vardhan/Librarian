0ebf50f4096b5a89a2b383f211a34a7c5cf72c20
ps_vardhan <peesarisaivardhan@gmail.com>
2026-06-27 00:51:04 +0530

    Old file removal

--- tests/test_config.py
+++
@@ -1,8 +0,0 @@
-from app.config import RAG_HOST, RAG_PORT, CHUNK_SIZE, CHUNK_OVERLAP, VECTOR_DB_TYPE
-
-def test_config_defaults():
-    assert RAG_HOST is not None
-    assert isinstance(RAG_PORT, int)
-    assert isinstance(CHUNK_SIZE, int)
-    assert isinstance(CHUNK_OVERLAP, int)
-    assert VECTOR_DB_TYPE is not None
