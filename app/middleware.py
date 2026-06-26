6d81f7e020baf12393fe3cced4bec7ef7a893013
ps_vardhan <peesarisaivardhan@gmail.com>
2026-06-27 00:59:33 +0530

    security JWT validation and access logs formatting

--- app/middleware.py
+++ app/middleware.py
@@ -1,23 +1,48 @@
-# app/middleware.py
+# ID-Rag/app/middleware.py
 import os
 import jwt
 from jwt import PyJWTError
 from fastapi import Request
 from datetime import datetime, timezone
 from fastapi.responses import JSONResponse
+from starlette.middleware.base import BaseHTTPMiddleware
 from app.config import logger
+
+HTTP_REQ = "http_req"
+HTTP_RES = "http_res"
+
+
+class LogMiddleware(BaseHTTPMiddleware):
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+
+        logger_method = logger.info
+
+        if str(request.url).endswith("/health"):
+            logger_method = logger.debug
+
+        logger_method(
+            f"Request {request.method} {request.url} - {response.status_code}",
+            extra={
+                HTTP_REQ: {"method": request.method, "url": str(request.url)},
+                HTTP_RES: {"status_code": response.status_code},
+            },
+        )
+
+        return response
 
 
 async def security_middleware(request: Request, call_next):
     async def next_middleware_call():
         return await call_next(request)
 
-    if request.url.path in {"/docs", "/openapi.json", "/health"}:
+    # Exclude root (frontend UI), docs, openapi, and health check from token checks
+    if request.url.path in {"/", "/docs", "/openapi.json", "/health"}:
         return await next_middleware_call()
 
     jwt_secret = os.getenv("JWT_SECRET")
     if not jwt_secret:
-        logger.warn("JWT_SECRET not found in environment variables")
+        logger.warning("JWT_SECRET not found in environment variables")
         return await next_middleware_call()
 
     authorization = request.headers.get("Authorization")
